b52dd0fbb2615aa06a164238b63d4e38c7aa8395
YUKESH <123308359+ANANDYUKESH@users.noreply.github.com>
2024-08-16 09:37:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-// function updateClock() {
-//     const now = new Date();
-//     const hours = now.getHours().toString().padStart(2, '0');
-//     const minutes = now.getMinutes().toString().padStart(2, '0');
-//     const seconds = now.getSeconds().toString().padStart(2, '0');
-//     const timeString = `${hours}:${minutes}:${seconds}`;
-    
-//     document.getElementById('clock').textContent = timeString;
-// }
-
-// setInterval(updateClock, 1000);
-// updateClock(); // initial call to display clock immediately
 function updateclock(){
  const now=new Date();
- const hours=now.getHours().toString().padStart(2, '0');
-//  const meridiem=hours>=12?"pm":"am";
-//  hours=hours% 12 || 12;
-//  hours=hours.toString().padStart(2, '0');
+ const hours=now.getHours();
+ const meridiem=hours>=12?"pm":"am";
+ hours=hours % 12 || 12;
+ hours=hours.toString().padStart(2, '0');
  const minutes=now.getMinutes().toString().padStart(2, '0');
  const seconds=now.getSeconds().toString().padStart(2, '0');
  const timeString=`${hours}:${minutes}:${seconds} `;
